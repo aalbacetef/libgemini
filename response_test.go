ceb3ee5121145eea9d1e8128e70c7f537ceaa879
Arturo Albacete <aalbacetef@gmail.com>
2024-02-01 17:49:06 +0100

    add test case; tidy up test code

--- response_test.go
+++ response_test.go
@@ -2,45 +2,76 @@ package libgemini
 
 import (
 	"bytes"
-	_ "embed"
 	"encoding/json"
+	"fmt"
+	"os"
 	"strings"
 	"testing"
 )
 
-//go:embed testdata/response.raw
-var testRawResponse []byte
-
-//go:embed testdata/response.json
-var testResponseJSON []byte
-
 func TestReadResponse(t *testing.T) {
-	r := bytes.NewReader(testRawResponse)
-
-	resp, err := ReadResponse(r)
-	if err != nil {
-		t.Fatalf("could not read response: %v", err)
+	cases := []struct {
+		label   string
+		rawResp []byte
+		want    []byte
+	}{
+		{
+			"response with MIME",
+			mustReadFile(t, "testdata/response.raw"),
+			mustReadFile(t, "testdata/response.json"),
+		},
+		{
+			"response no MIME",
+			mustReadFile(t, "testdata/response.no-mime.raw"),
+			mustReadFile(t, "testdata/response.no-mime.json"),
+		},
 	}
 
-	want := trim(testResponseJSON)
+	for _, c := range cases {
+		t.Run(c.label, func(tt *testing.T) {
+			r := bytes.NewReader(c.rawResp)
 
-	got, err := json.MarshalIndent(resp, "", "  ")
-	if err != nil {
-		t.Fatalf("could not encode JSON: %v", err)
-	}
+			resp, err := ReadResponse(r)
+			if err != nil {
+				tt.Fatalf("could not read response: %v", err)
+			}
 
-	m, n := len(got), len(want)
-	if m != n {
-		t.Fatalf("(length) got %d bytes, want %d", m, n)
-	}
+			want := trim(c.want)
 
-	for k, b := range got {
-		if b != want[k] {
-			t.Fatalf("responses differ. got.Content: %s", string(resp.Content))
-		}
+			got, err := json.MarshalIndent(resp, "", "  ")
+			if err != nil {
+				tt.Fatalf("could not encode JSON: %v", err)
+			}
+
+			wantR := Response{}
+			_ = json.Unmarshal(want, &wantR)
+
+			m, n := len(got), len(want)
+			if m != n {
+				tt.Fatalf("(length) got %d bytes, want %d", m, n)
+			}
+
+			for k, b := range got {
+				if b != want[k] {
+					fmt.Println(wantR.Content)
+					tt.Fatalf("responses differ. got.Content: '%s', want: '%s'", string(resp.Content), string(wantR.Content))
+				}
+			}
+		})
 	}
 }
 
 func trim(b []byte) []byte {
 	return []byte(strings.TrimSpace(string(b)))
+}
+
+func mustReadFile(t *testing.T, fpath string) []byte {
+	t.Helper()
+
+	data, err := os.ReadFile(fpath)
+	if err != nil {
+		t.Fatalf("could not read '%s': %v", fpath, err)
+	}
+
+	return data
 }
